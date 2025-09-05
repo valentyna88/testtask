b0dcec2c4a6ec211f00fdba47a401b0929dc6c99
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2025-09-06 01:53:12 +0300

    feat: add UserCard component

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -1,4 +1,30 @@
-const UserCard = () => {
-  return <div>UserCard</div>;
+const UserCard = ({ user }) => {
+  const { photo, name, position, email, phone } = user;
+  return (
+    <>
+      <div className="users-wrapper">
+        <img
+          src={photo}
+          alt={name}
+          className="users-photo"
+          width={70}
+          height={70}
+        />
+
+        <p className="users-text ellipsis-1" title={name}>
+          {name}
+        </p>
+        <div>
+          <p className="users-text ellipsis-1" title={position}>
+            {position}
+          </p>
+          <p className="users-text ellipsis-1" title={email}>
+            {email}
+          </p>
+          <p className="users-text">{phone}</p>
+        </div>
+      </div>
+    </>
+  );
 };
 export default UserCard;
